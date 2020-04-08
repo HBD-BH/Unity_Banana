8476fa176797d3736c88ebf66b99371908fa28b2
Bernhard Häfner <bernhardhaefner@hotmail.com>
2020-04-08 10:40:37 +0200

    Upload solution including documentation.

---
+++ model.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    def __init__(self, input_size, output_size, seed, hidden_layers=[64,64]):
+        ''' Builds a feedforward network with arbitrary hidden layers.
+        
+            Arguments
+            ---------
+            input_size: integer, size of the input (e.g., state space)
+            output_size: integer, size of the output layer (e.g., action space)
+            seed (int): Random seed
+            hidden_layers: list of integers, the sizes of the hidden layers
+        '''
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+        # Add the first layer, input to a hidden layer
+        self.hidden_layers = nn.ModuleList([nn.Linear(input_size, hidden_layers[0])])
+        
+        # Add a variable number of more hidden layers
+        layer_sizes = zip(hidden_layers[:-1], hidden_layers[1:])
+        self.hidden_layers.extend([nn.Linear(h1, h2) for h1, h2 in layer_sizes])
+        
+        self.output = nn.Linear(hidden_layers[-1], output_size)
+        
+    def forward(self, x):
+        ''' Forward pass through the network, returns the output logits '''
+        
+        # Forward through each layer in `hidden_layers`, with ReLU activation and dropout
+        for linear in self.hidden_layers:
+            x = F.relu(linear(x))
+        
+        x = self.output(x)
+        
+        return x
+    
